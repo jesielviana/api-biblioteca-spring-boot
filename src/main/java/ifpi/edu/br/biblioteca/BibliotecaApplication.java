a58f49237ccf7421ffc3fb6e4da33d4f02016db9
Jesiel Viana <jesielviana@proton.me>
2024-12-14 16:05:34 -0300

    add rota para /

--- src/main/java/ifpi/edu/br/biblioteca/BibliotecaApplication.java
+++ src/main/java/ifpi/edu/br/biblioteca/BibliotecaApplication.java
@@ -2,12 +2,20 @@ package ifpi.edu.br.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return String.format("Biblioteca online");
 	}
 
 }
